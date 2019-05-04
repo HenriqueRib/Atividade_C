97970447778a4d43546ed208b13f792b45dfbdcd
Henrique Ribeiro <47700931+HenriqueRib@users.noreply.github.com>
2019-05-04 18:18:27 -0300

    Ordenação de vetores
    Nesse código fonte é possível ordenar um vetor em ordem cronológica .

---
+++ Ordenacao vetor.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main()
+{
+	int vetorA[10];
+	int i,j, troca;
+	
+	for (i=0;i<10;i++)
+	{
+		printf("Digite o %d elemento: ", i);
+		scanf("%d", &vetorA[i]);
+	}
+	for (i=0; i<9; i++)
+	   {
+			for (j=i+1; j<10;j++)
+			{
+				if (vetorA[i] > vetorA[j])
+				{
+					troca = vetorA[i];
+					vetorA[i] = vetorA[j];
+					vetorA[j] = troca ;
+				}
+			}
+	   }
+	printf ("\n Vetor ordenado e \n");
+	for (i=0;i<10; i++)
+		{
+			printf("%d - ", vetorA[i]);
+		}
+		system("pause"); 
+	return (0);
+}
